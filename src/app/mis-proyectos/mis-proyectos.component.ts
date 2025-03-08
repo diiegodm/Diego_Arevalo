b51960ca478c6b621f637135d6062d60edc8b137
diiegodm <diegoarevalodm@gmail.com>
2025-03-08 01:25:10 +0100

    todo sobre mi

---
+++ src/app/mis-proyectos/mis-proyectos.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-proyectos',
+  imports: [CommonModule],
+  templateUrl: './mis-proyectos.component.html',
+  styleUrl: './mis-proyectos.component.css'
+})
+export class MisProyectosComponent {
+ 
+}
